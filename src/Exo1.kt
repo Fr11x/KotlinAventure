494f5a30a9ecb1138c64f9d5aa598c414327f8dd
momika <fmomika@gmail.com>
2024-09-19 15:47:16 +0200

    fin dex exo

--- src/Exo1.kt
+++ src/Exo1.kt
@@ -1,147 +1,206 @@
-
-fun calculDefenseTotal(defense: Int, typeArmure: Int, rarete: Int): Int = defense+typeArmure+rarete
+fun calculDefenseTotal(defense: Int, typeArmure: Int, rarete: Int): Int = defense + typeArmure + rarete
 
 fun lanceDes(nombreDes: Int, nombreFace: Int): Int {
-              var res = 0
-              for (i in 1..nombreDes) {
-                  res += (1..nombreFace).random()
-              }
-              return res
-          }
-fun calculDegatArme(nbDes: Int,nbFace: Int,bonus: Int,activeCrit: Int, mutliplicateurCrit: Int): Int{
-    var resDes = lanceDes(nbDes,nbFace)
-    if (resDes >= activeCrit){
+    var res = 0
+    for (i in 1..nombreDes) {
+        res += (1..nombreFace).random()
+    }
+    return res
+}
+
+fun calculDegatArme(nbDes: Int, nbFace: Int, bonus: Int, activeCrit: Int, mutliplicateurCrit: Int): Int {
+    var resDes = lanceDes(nbDes, nbFace)
+    if (resDes >= activeCrit) {
         resDes *= mutliplicateurCrit
         resDes += bonus
     }
-    if (resDes <= 0){
+    if (resDes <= 0) {
         resDes = 0
     }
     return resDes
 }
-fun attaque(pv: Int,def: Int, degat: Int, nomAttaquant: String, nomCible: String){
-    var armeDegat = degat-def
-    var ciblePv = pv
-    if (armeDegat <= 0){
+
+fun attaque(pv: Int, def: Int, degat: Int, nomAttaquant: String, nomCible: String) {
+    var armeDegat = degat - def
+    if (armeDegat <= 0) {
         armeDegat = 0
     }
-    ciblePv = pv - armeDegat
-    if (ciblePv <0){
-        ciblePv = 0
+    var pvActuelle = pv - armeDegat
+    if (pvActuelle < 0){
+        pvActuelle = 0
     }
     println("$nomAttaquant attaque $nomCible pour $armeDegat point de dégats")
 }
-fun boirePotion(nomPerso: String, pv: Int, pvMax: Int, puissancePotion: Int){
+
+fun boirePotion(nomPerso: String, pv: Int, pvMax: Int, puissancePotion: Int) {
     val exPv = pv
     var newPv = pv + puissancePotion
-    if (newPv > pvMax){
+    if (newPv > pvMax) {
         newPv = pvMax
     }
     val recup = newPv - exPv
     println("$nomPerso boit une potion et récupère $recup PV")
 }
 
-fun bouleDeFeu(nomCaster: String,nomCible: String,scoreAttaqueCaster: Int,defCible: Int, pvCible: Int){
+fun bouleDeFeu(nomAttaquant: String, nomCible: String, degat: Int, def: Int, pvCible: Int) {
     val pvBase = pvCible
-    var degat = (lanceDes(scoreAttaqueCaster/3,6)) - defCible
+    var degat = (lanceDes(degat / 3, 6)) - def
     var pvActuelle = pvCible
-    if (degat < 0){
+    if (degat < 0) {
         degat = 0
     }
     pvActuelle -= degat
-    if (pvActuelle < 0 ){
+    if (pvActuelle < 0) {
         pvActuelle = 0
     }
-    val pvPerdu = pvCible -pvActuelle
-    println("$nomCaster lance une boule de feu et inflige $pvPerdu points de dégâts à $nomCible.")
+    val pvPerdu = pvCible - pvActuelle
+    println("$nomAttaquant lance une boule de feu et inflige $pvPerdu points de dégâts à $nomCible.")
 
 }
-fun missileMagique(nomCaster: String,nomCible: String,scoreAttaqueCaster: Int,def: Int,pvCible: Int){
+
+fun missileMagique(nomAttaquant: String, nomCible: String, degat: Int, def: Int, pvCible: Int) {
     var pvActuelle = pvCible
-    for (i in 1..scoreAttaqueCaster/2){
-        var missile = lanceDes(1,6) - def
-        if (missile < 0){
+    for (i in 1..degat / 2) {
+        var missile = lanceDes(1, 6) - def
+        if (missile < 0) {
             missile = 0
         }
         pvActuelle -= missile
-        if (pvActuelle <= 0){
+        if (pvActuelle <= 0) {
             pvActuelle = 0
             var pvPerdu = pvCible - pvActuelle
-            println("$nomCaster lance une missile magique et inflige $pvPerdu points de dégâts à $nomCible.")
+            println("$nomAttaquant lance une missile magique et inflige $pvPerdu points de dégâts à $nomCible.")
             break
         }
         var pvPerdu = pvCible - pvActuelle
-        println("$nomCaster lance une missile magique et inflige $pvPerdu points de dégâts à $nomCible.")
-        }
-    }
-fun invocationArme(nomCaster: String,typeArme: String){
-    val lance = lanceDes(1,20)
-    val rarete = if (lance<5) "commune" else if (lance < 10) "rare" else if (lance < 15) "épic" else "légendaire"
-    println("$nomCaster invoque un(e) $typeArme $rarete")
-    }
-fun soins(nomCaster: String,scoreAttaqueCaster: Int,pvCible: Int,pvMax: Int,isDead: Boolean){
-    val soin = scoreAttaqueCaster/2
-    var pvAcutelle = pvCible
-    if (isDead){
-        pvAcutelle -= soin
-        if (pvAcutelle < 0){
-            pvAcutelle =0
-        }
-        val pvPerdu = pvCible -pvAcutelle
-        println("$nomCaster a infligé $pvPerdu de dégats")
-    }
-    else{
-        pvAcutelle += soin
-        if (pvAcutelle >pvMax){
-            pvAcutelle = pvMax
-        }
-        val pvGagne = pvAcutelle-pvCible
-        println("$nomCaster a récupérer $pvGagne PV")
+        println("$nomAttaquant lance une missile magique et inflige $pvPerdu points de dégâts à $nomCible.")
     }
 }
-fun afficherInventaire(nomPerso: String, inventaires: MutableList<String>){
+
+fun invocationArme(nomAttaquant: String, typeArme: String) {
+    val lance = lanceDes(1, 20)
+    val rarete = if (lance < 5) "commune" else if (lance < 10) "rare" else if (lance < 15) "épic" else "légendaire"
+    println("$nomAttaquant invoque un(e) $typeArme $rarete")
+}
+
+fun soins(nomAttaquant: String, degat: Int, pvCible: Int, pvMax: Int, isDead: Boolean) {
+    val soin = degat / 2
+    var pvAcutelle = pvCible
+    if (isDead) {
+        pvAcutelle -= soin
+        if (pvAcutelle < 0) {
+            pvAcutelle = 0
+        }
+        val pvPerdu = pvCible - pvAcutelle
+        println("$nomAttaquant a infligé $pvPerdu de dégats")
+    } else {
+        pvAcutelle += soin
+        if (pvAcutelle > pvMax) {
+            pvAcutelle = pvMax
+        }
+        val pvGagne = pvAcutelle - pvCible
+        println("$nomAttaquant a récupérer $pvGagne PV")
+    }
+}
+
+fun afficherInventaire(nomPerso: String, inventaires: MutableList<String>) {
     println("inventaire de $nomPerso")
-    for (inventaire in inventaires.indices){
+    for (inventaire in inventaires.indices) {
         println("$inventaire => ${inventaires[inventaire]}")
     }
 }
-fun choisirItem(nomPerso: String,inventaires: MutableList<String>){
-    val inv = afficherInventaire(nomPerso,inventaires)
+
+fun choisirItem(nomPerso: String, inventaires: MutableList<String>) {
+    val inv = afficherInventaire(nomPerso, inventaires)
     println("choisissez l'item")
     var i = 0
-    while (i <= 1){
+    while (i <= 1) {
         var res = readln().toInt()
-        if (res in inventaires.indices){
+        if (res in inventaires.indices) {
             println("$nomPerso utilise ${inventaires[res]}")
             i++
-        }
-        else{
+        } else {
             println("valeur non valide. veuillez recommencez")
-            val inv = afficherInventaire(nomPerso,inventaires)
+            val inv = afficherInventaire(nomPerso, inventaires)
         }
     }
 }
-fun tourJoueur(defense: Int, typeArmure: Int, rarete: Int, nombreDes: Int, nombreFace: Int, bonus: Int,activeCrit: Int,
-               mutliplicateurCrit: Int,def: Int, degat: Int, nomAttaquant: String, nomCible: String,nomPerso: String,
-               pv: Int, pvMax: Int, puissancePotion: Int,nomCaster: String,nomCible: String,scoreAttaqueCaster: Int,defCible: Int, pvCible: Int,
-               ){
+
+fun tourJoueur(
+    def: Int, degat: Int, nomAttaquant: String, nomCible: String, nomPerso: String,
+    pv: Int, pvMax: Int, pvCible: Int,
+    isDead: Boolean, inventaires: MutableList<String>
+) {
+
     println("veuillez choisir une action")
-    val actions = listOf("Attaquer","Boule de feu", "Missile Magique", "Soins","Utiliser un item")
-    for (choix in actions.indices){
+    val actions = listOf("Attaquer", "Boule de feu", "Missile Magique", "Soins", "Utiliser un item")
+    for (choix in actions.indices) {
         println("$choix => ${actions[choix]}")
     }
-    var res  = readln().toInt()
-    when(res){
-        0 -> attaque()
-        1 -> bouleDeFeu()
-        2 -> missileMagique()
-        3 -> soins()
-        4 -> choisirItem()
+    var i = 0
+    while (i < 1) {
+        var res = readln().toInt()
+        when (res) {
+            0 -> {
+                attaque(pv, def, degat, nomAttaquant, nomCible)
+                i++
+            }
+
+            1 -> {
+                bouleDeFeu(nomAttaquant, nomCible, degat, def, pvCible)
+                i++
+            }
+
+            2 -> {
+                missileMagique(nomAttaquant, nomCible, degat, def, pvCible)
+                i++
+            }
+
+            3 -> {
+                soins(nomAttaquant, degat, pvCible, pvMax, isDead)
+                i++
+            }
+
+            4 -> {
+                choisirItem(nomPerso, inventaires)
+            }
+
+            else -> println("recommencez")
+        }
     }
 
 }
 
- fun main(){
+fun tourOrdinateur(
+    def: Int, degat: Int, nomAttaquant: String, nomCible: String, nomPerso: String,
+    pv: Int, pvMax: Int,pvCible: Int,
+    isDead: Boolean, inventaires: MutableList<String>
+) {
+    val lance = lanceDes(1, 30)
+    val actions = listOf("Attaquer", "Boule de feu", "Missile Magique", "Soins", "Utiliser un item")
+    for (choix in actions.indices) {
+        println("$choix => ${actions[choix]}")
+    }
+    when (lance) {
+        in 1..14 -> attaque(pv, def, degat, nomAttaquant, nomCible)
+        in 15..19 -> bouleDeFeu(nomAttaquant, nomCible, degat, def, pvCible)
+        in 20..24 -> missileMagique(nomAttaquant, nomCible, degat, def, pvCible)
+        else -> soins(nomAttaquant, degat, pvCible, pvMax, isDead)
+
+    }
+}
+fun tourCombat(
+    def: Int, degat: Int, nomAttaquantJ: String, nomCibleJ: String, nomPersoJ: String,
+    pv: Int, pvMax: Int,pvCible: Int, nomAttaquantO: String, nomCibleO: String, nomPersoO: String,
+    isDead: Boolean, inventaires: MutableList<String>){
+    println("Début du tour")
+    tourJoueur(def,degat,nomAttaquantJ,nomCibleJ,nomPersoJ,pv,pvMax,pvCible,true, mutableListOf("épee","pioche","carotte"))
+    tourOrdinateur(def,degat,nomAttaquantO,nomCibleO,nomPersoO,pv,pvMax,pvCible,true, mutableListOf("épee","pioche","carotte"))
+    println("Fin du tour")
+}
+
+
+fun main() {
 //     println(calculDefenseTotal(3,2,1))
 //     val unDes6 = lanceDes(1,6)
 //     val deuxDes5 = lanceDes(2,5) val quatreDes20 = lanceDes(4,20)
@@ -158,5 +217,15 @@ fun tourJoueur(defense: Int, typeArmure: Int, rarete: Int, nombreDes: Int, nombr
 //     invocationArme("François","épee")
 //     soins("François",30,10,100,false)
 //     afficherInventaire("Ligma", mutableListOf("crampte","rizz","lookmaxing"))
-     choisirItem("Ligma",mutableListOf("crampte","rizz","lookmaxing"))
-          }
+//     choisirItem("Ligma",mutableListOf("crampte","rizz","lookmaxing"))
+//    tourJoueur(
+//        6, 20, "François", "Isma", "François", 30, 120,
+//        40, false, mutableListOf("épee", "pioche", "pelle", "or")
+//    )
+//    tourOrdinateur(
+//        6, 20, "François", "Isma", "François", 30, 120,
+//        40, false, mutableListOf("épee", "pioche", "pelle", "or")
+//    )
+    tourCombat(6, 20, "François", "Ordinateur", "François", 30, 120,
+        40, "Ordinateur","Joueur","Ordinateur",true,mutableListOf("épee", "pioche", "pelle", "or"))
+}
